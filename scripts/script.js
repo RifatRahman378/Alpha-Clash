0ce807024b07ce7a741157aeb066c5ed4bc7cf6a
rifat.rahman378@gmail.com <rifat.rahman378@gmail.com>
2024-04-18 19:43:32 +0600

    keyboard capture

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,23 @@ function playNow() {
     show('play-ground');
     gameLoop();
 }
-function keyboardCapture() {
-console.log('keyboardCaptured');
+function keyboardCapture(event) {
+    const pressed = event.key;
+    const alphabet = document.getElementById('current-alphabet').innerText;
+    const currentAlphabet = alphabet.toLowerCase(); 
+    console.log('Current Alphabet:', currentAlphabet);
+    console.log('Button pressed:', pressed);
+    if (currentAlphabet === pressed) {
+        console.log('right button pressed');
+        removeBackgroudColor(currentAlphabet);
+        gameLoop();
+    }
+    else{
+        console.log('wrong button pressed');
+    }
+    
 }
+
 document.addEventListener('keyup', keyboardCapture);
 function hide(elementId) {
     const element = document.getElementById(elementId);
@@ -19,11 +33,16 @@ function backgroundColor(elementId) {
     const element = document.getElementById(elementId)
     element.classList.add('bg-orange-400');
 }
+function removeBackgroudColor(elementId){
+    const element = document.getElementById(elementId)
+    element.classList.remove('bg-orange-400');
+}
 function gameLoop() {
     const alphabet = randomAlphabet();
     const element = document.getElementById('current-alphabet');
     element.innerText = alphabet;
-    backgroundColor(alphabet)
+    backgroundColor(alphabet);
+    
 }
 function randomAlphabet() {
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz'
